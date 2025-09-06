2237fa92ff27198e7e1c9f7d815c21bc5f29fb9f
Joanna Amoah <joannaea@udel.edu>
2025-09-06 18:44:19 -0400

    added a header and image, and changed the bg to red

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+import image from "./silly_owl.jpg";
 
 function App(): React.JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Joanna Amoah Hello World
+                UD CISC275 with React Hooks and TypeScript Joanna Amoah Hello
+                World
             </header>
+            <h1> Header text :D</h1>
+            <img src={image} alt="Silly Owl" />
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
