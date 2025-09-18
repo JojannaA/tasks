ef8ac1038451444922585f3ca1a89ce892b13fc8
Joanna Amoah <joannaea@udel.edu>
2025-09-18 19:03:27 -0400

    function toShortForm is complete

--- src/objects.ts
+++ src/objects.ts
@@ -60,7 +60,11 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    if (question.name.length <= 10) {
+        return `${question.id}: ${question.name}`;
+    } else {
+        return `${question.id}: ${question.name.slice(0, 10)}`;
+    }
 }
 
 /**
