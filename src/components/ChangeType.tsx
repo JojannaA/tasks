0554eaf5c290029f0833378feb1677e26bb3a25b
Joanna Amoah <joannaea@udel.edu>
2025-09-25 14:51:09 -0400

    added a button where it'll change the type of question questioned.... yea

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,20 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): React.JSX.Element {
-    return <div>Change Type</div>;
+    const [type, setType] = useState<QuestionType>("short_answer_question");
+    function changeType(): void {
+        type === "short_answer_question" ?
+            setType("multiple_choice_question")
+        :   setType("short_answer_question");
+    }
+    return (
+        <div>
+            <div>
+                {type === "short_answer_question" ?
+                    "Short Answer"
+                :   "Multiple Choice"}
+            </div>
+            <Button onClick={changeType}>Change Type</Button>
+        </div>
+    );
 }
