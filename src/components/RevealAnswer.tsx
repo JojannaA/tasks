7aa99818b2a322cedaf7d40752aaacd4c23335f2
Joanna Amoah <joannaea@udel.edu>
2025-09-25 13:48:30 -0400

    added a button that reveals the answer

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,16 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [show, setShow] = useState<boolean>(false);
+
+    function toggleAnswer(): void {
+        setShow(!show);
+    }
+
+    return (
+        <div>
+            <Button onClick={toggleAnswer}>Reveal Answer</Button>{" "}
+            {show && <div>42</div>}
+        </div>
+    );
 }
