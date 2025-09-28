1fa2e6ef1ce74117a9b5ba5e8354225080e98d9f
Joanna Amoah <joannaea@udel.edu>
2025-09-28 19:41:21 -0400

    uncommented DoubleHalf

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ function App(): React.JSX.Element {
                 World
             </header>
             <hr></hr>
-            {/* <DoubleHalf></DoubleHalf> */}
+            <DoubleHalf></DoubleHalf>
             <hr></hr>
             <ChooseTeam></ChooseTeam>
             <hr></hr>
